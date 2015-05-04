2f32ecd7eb03e73671af0413eb7559770e1cc492
leechannl <leechannl@gmail.com>
2015-05-04 11:11:55 +0800

    Layout the mocked data.

--- index.ios.js
+++ index.ios.js
@@ -4,9 +4,15 @@
  */
 'use strict';
 
+// Mock some data and get hands dirty
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
@@ -14,18 +20,17 @@ var {
 
 var ReactNativeDemo = React.createClass({
   render: function() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
+        <Image
+        source={{uri: movie.posters.thumbnail}}
+        style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+        <Text style={styles.title}>{movie.title}</Text>
+        <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -34,19 +39,25 @@ var ReactNativeDemo = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
-  instructions: {
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
